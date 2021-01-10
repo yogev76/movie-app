325bc7238da9c7c553ba7d7ae9df5d7e0dd54730
Yogev Etz Haim <yogev76@gmail.com>
2021-01-10 14:22:20 +0200

    getting started

---
+++ src/components/ActorsView/ActorsView.js
@@ -0,0 +1,18 @@
+import { ReactComponent } from '*.svg'
+import React from 'react'
+import { Table } from 'react-bootstrap'
+
+class ActorsView extends React.Component {
+
+    render () {
+        return (
+            <Table>
+     
+         </Table>
+ 
+        )
+    }
+    
+}
+
+export default ActorsView
